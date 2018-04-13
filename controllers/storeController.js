9ae802181a33db6c4959fcab82e6d6ec28284f97
Nate <nate@natewelling.com>
2018-04-13 20:21:52 +0800

    add editStore route, lat & longitude inputs

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -26,3 +26,39 @@ exports.createStore = async (req, res) => {
   req.flash('success', `Successfully created ${store.name}.  Care to leave a review?`);
   res.redirect(`/store/${store.slug}`);
 };
+
+exports.getStores = async (req, res) => {
+  const stores = await Store.find();
+  res.render('stores', {
+    title: 'Stores',
+    stores,
+  });
+};
+
+exports.editStore = async (req, res) => {
+  // 1. Find the store given id
+  const store = await Store.findOne({ _id: req.params.id });
+  // 2. Confirm user is owner of store
+  // 3. Render out edit form so user can update
+  res.render('editStore', {
+    title: `Edit ${store.name}`,
+    store,
+  });
+};
+
+exports.updateStore = async (req, res) => {
+  // 1. Find store
+  const store = await Store.findOneAndUpdate(
+    {
+      _id: req.params.id,
+    },
+    req.body,
+    {
+      new: true,
+      runValidators: true,
+    }
+  ).exec();
+  req.flash('success', `Successfully updated ${store.name}. <a href="/stores/${store.slug}">View store -> </a>`);
+  res.redirect(`/stores/${store._id}/edit`);
+  // 2. Redirect to store & flash success
+};
