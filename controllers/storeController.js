e4677a24084d71587de0715f1138875d445f721d
Nate <nate@natewelling.com>
2018-04-14 17:35:41 +0800

    refactor getStoresByTag to Promise.all for multiple queries

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -110,11 +110,15 @@ exports.getStoreBySlug = async (req, res, next) => {
 };
 
 exports.getStoresByTag = async (req, res) => {
-  const tags = await Store.getTagsList();
   const tag = req.params.tag;
+  const tagQuery = tag || { $exists: true };
+  const tagsPromise = Store.getTagsList();
+  const storesPromise = Store.find({ tags: tagQuery });
+  const [tags, stores] = await Promise.all([tagsPromise, storesPromise]);
   res.render('tags', {
     title: `tags`,
     tags,
     tag,
+    stores,
   });
 };
