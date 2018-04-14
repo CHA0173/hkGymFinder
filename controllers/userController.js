b9989172d6e39214753d993c064ca4ba9889b66c
Nate <nate@natewelling.com>
2018-04-15 00:43:21 +0800

    add user login/registration routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 
+const User = mongoose.model('User');
+const promisify = require('es6-promisify');
+
 exports.loginForm = (req, res) => {
   res.render('login', { title: 'Login' });
 };
@@ -26,5 +29,16 @@ exports.validateRegister = (req, res, next) => {
     res.render('register', { title: 'Register', body: req.body, flashes: req.flash() });
     return;
   }
+  next();
+};
+
+exports.register = async (req, res, next) => {
+  const user = new User({
+    email: req.body.email,
+    name: req.body.name,
+    password: req.body.password,
+  });
+  const registerWithPromise = promisify(User.register, User);
+  await registerWithPromise(user, req.body.password);
   next();
 };
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
 const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 
 const { catchErrors } = require('../handlers/errorHandlers');
 // Do work here
@@ -33,6 +34,6 @@ router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
 
 router.get('/login', userController.loginForm);
 router.get('/register', userController.registerForm);
-router.post('/register', userController.validateRegister, userController.registerForm);
+router.post('/register', userController.validateRegister, userController.register, authController.login);
 
 module.exports = router;
